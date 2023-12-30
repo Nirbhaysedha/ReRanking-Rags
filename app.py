fb7be29b07a693ff9da3b2b7c507f1ab0abe4943
NirbhaySedha <Sedha9nirbhay@gmail.com>
2023-12-30 13:32:37 +0530

    Main Model Pushed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import json
 
 def get_result(query, passages, choice):
+    # choices between multiple language models according to problem statement 
     if choice == "Nano":
         ranker = Ranker()
     elif choice == "Small":
@@ -22,13 +23,49 @@ st.set_page_config(layout="wide",
                    page_title="Lost in the middle issue ! Solving ")
 
 
-
 def main():
-    st.title("Re Ranking by Flash Rank")
-    st.sidebar.write("According to model size choose ")
-    menu=["Nano","Small","Medium","Large"]
-    choice=st.sidebar.selectbox("Choose",menu)
+    st.title("ReRanking using Flash Rank")
+    st.sidebar.write("According to the Model Size 👇")
+    menu = ["Nano", "Small", "Medium", "Large"]
+    choice = st.sidebar.selectbox("Choose", menu)
+
+    st.sidebar.info("""
+**Model Options:**
+- **Nano**: ~4MB, blazing fast model with competitive performance (ranking precision).
+- **Small**: ~34MB, slightly slower with the best performance (ranking precision).
+- **Medium**: ~110MB, slower model with the best zero-shot performance (ranking precision).
+- **Large**: ~150MB, slower model with competitive performance (ranking precision) for 100+ languages.
+""")
+
+    with st.expander("About Flash Rank"):
+        st.markdown("""
+        **Flash Rank**: Ultra-lite & Super-fast Python library for search & retrieval re-ranking.
+
+        - **Ultra-lite**: No heavy dependencies. Runs on CPU with a tiny ~4MB reranking model.
+        - **Super-fast**: Speed depends on the number of tokens in passages and query, plus model depth.
+        - **Cost-efficient**: Ideal for serverless deployments with low memory and time requirements.
+        - **Based on State-of-the-Art Cross-encoders**: Includes models like ms-marco-TinyBERT-L-2-v2 (default), ms-marco-MiniLM-L-12-v2, rank-T5-flan, and ms-marco-MultiBERT-L-12.
+        - **Sleek Models for Efficiency**: Designed for minimal overhead in user-facing scenarios.
+
+        _Flash Rank is tailored for scenarios requiring efficient and effective reranking, balancing performance with resource usage._
+        """)
+
+    with st.form(key='input_form'):
+        query_input = st.text_area("Query Input")
+        context_input = st.text_area("Context Input")
+        submit_button = st.form_submit_button(label='ReRank')
+
+    if submit_button:
+        with st.spinner('Processing...'):
+            passages = json.loads(context_input)
+            print("passages: ", passages, type(passages))
+            result = get_result(query_input, passages, choice)
+            st.subheader("Please find the ReRanked results below 👇")
+            st.json(result)
 
 
-main()
+
+if __name__ == "__main__":
+    main()
+
 
